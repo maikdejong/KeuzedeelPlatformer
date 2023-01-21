4e8232e59812e2c938f1282f97c2b55987e08aa5
maikdejong <82510647+maikdejong@users.noreply.github.com>
2023-01-21 14:52:28 +0100

    Eindscene
    Eindscene toegevoegd, waar je de keuze hebt om de game af te sluiten, of opnieuw te spelen.
    Play again brengt je naar de Main menu

--- Assets/Scripts/Core/MainMenu.cs
+++ Assets/Scripts/Core/MainMenu.cs
@@ -10,6 +10,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
     public void QuitGame()
     {
         Debug.Log("QUIT");
